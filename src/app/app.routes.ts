daa46192e6e80d782d6257e7426ae5e27ac3afc7
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-05-08 15:42:05 -0500

    Privacy-Policy working!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './register/register.component';
 import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'password-recovery', component: PasswordRecoveryComponent},
     { path: 'page-not-found', component: PageNotFoundComponent},
     { path: 'home', component: HomeComponent},
+    { path: 'privacy-policy', component:PrivacyPolicyComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' }, // Ruta por defecto, redirige a login
     { path: '**', redirectTo: '/page-not-found' },
 ];
---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SquirrelLogoComponent } from '../squirrel-logo/squirrel-logo.component';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [SquirrelLogoComponent],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+
+}
